29bfeb6d5adca17d8dcf5dbc76422382b19871cb
alichoopani <alichoopani1380@gmail.com>
2021-01-30 18:08:48 +0330

    change some names

--- khayyam_pascal_triangle/KhayyamPascalTriangle.java
+++ khayyam_pascal_triangle/KhayyamPascalTriangle.java
@@ -7,18 +7,18 @@ public class KhayyamPascalTriangle
 		Scanner sc = new Scanner(System.in);
 		System.out.print("insert number: ");
 		Integer input = sc.nextInt();
-		printZhayam(input);
+		printKhayyam(input);
 	}
 
-	private static void printZhayam(Integer i) {
+	private static void printKhayyam(Integer i) {
 		for (int j = 1; j <= i; j++) {
 			for (int k = 1; k <= j; k++) 
-				System.out.print(getZhayam(k, j) + " ");
+				System.out.print(getKhayyam(k, j) + " ");
 			System.out.println();
 		}
 	}
 
-	private static Integer getZhayam(Integer i, Integer j) {
-		return (i == 1 || i == j) ? 1 : getZhayam(i - 1, j - 1) + getZhayam(i, j - 1);
+	private static Integer getKhayyam(Integer i, Integer j) {
+		return (i == 1 || i == j) ? 1 : getKhayyam(i - 1, j - 1) + getKhayyam(i, j - 1);
 	}
 }
